5d3757a10b3e303f41820b1e3e3bc728634b5501
Oleg Verkhoturov <overkhoturov@mentalstack.com>
2022-02-11 13:02:28 +0300

    Add Header component

---
+++ components/Header/index.tsx
@@ -0,0 +1,70 @@
+import { useState } from 'react'
+import {Box, Avatar, Menu, MenuItem, Button, Container} from '@mui/material'
+import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown';
+
+interface HeaderProps {
+  logoUrl: string,
+  profileName: string,
+  menuLabel: string
+}
+
+const Header = ({logoUrl, profileName, menuLabel}: HeaderProps) => {
+  const [anchorEl, setAnchorEl] = useState(null);
+  const open = Boolean(anchorEl);
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  return (
+    <Box sx={{
+      borderBottom: '1px solid #9e9e9e',
+      paddingBottom: '20px'
+    }} >
+      <Container>
+        <Box sx={{
+          borderBottom: '1px solid #9e9e9e',
+          padding: '35px 0'
+        }}>
+          {logoUrl && <img className="news__logo" src={logoUrl} />}
+        </Box>
+        <Box sx={{
+          display: 'flex',
+          justifyContent: 'space-between',
+          marginTop: '20px'
+        }} className="news__header">
+          <span>{menuLabel}</span>
+          <Box sx={{display: 'flex'}}>
+            <Avatar sx={{
+              bgcolor: 'gray'
+            }} />
+            <Button 
+              endIcon={<ArrowDropDownIcon />}
+              aria-controls={open ? 'basic-menu' : undefined}
+              aria-haspopup="true"
+              aria-expanded={open ? 'true' : undefined}
+              onClick={handleClick}
+            >
+              <span>{profileName}</span>
+            </Button>
+          </Box>
+          <Menu
+            id="basic-menu"
+            anchorEl={anchorEl}
+            open={open}
+            onClose={handleClose}
+            MenuListProps={{
+              'aria-labelledby': 'basic-button',
+            }}
+          >
+            <MenuItem onClick={handleClose}>Sign out</MenuItem>
+          </Menu>
+        </Box>
+      </Container>
+    </Box>
+  )
+} 
+
+export default Header
--- pages/news/index.tsx
+++ pages/news/index.tsx
@@ -1,22 +1,71 @@
 import { useEffect, useState } from 'react';
-import {AssetCollection} from 'contentful'
+import {Container, Box, Typography} from '@mui/material'
+import {Entry} from 'contentful'
 
+import Header from '../../components/Header'
 import contentFullClient from '../../lib/contentfulService'
 
+interface Fields {
+  logo: {
+    sys: {
+      id: string,
+      linkType: string,
+      type: string
+    }
+  },
+  menuLabel: string,
+  searchLabel: string,
+  title: string
+}
+
 const News = () => {
-  const [pageData, setPageData] = useState<AssetCollection>()
+  const [searchLabel, setSearchLabel] = useState<string>('')
+  const [menuLabel, setMenuLabel] = useState<string>('')
+  const [logoId, setLogoId] = useState<string>('')
+  const [logoUrl, setLogoUrl] = useState<string>('')
 
   useEffect(() => {
-    contentFullClient.getContentType('newsConfig')
-    .then((result) => console.log(result))
-    .catch((err) => console.log(err));
-
+    contentFullClient.getEntries({ content_type: 'newsConfig' })
+      .then((result) => {
+        const [item] = result.items
+        const {fields} = item as Entry<Fields>;
+        setSearchLabel(fields.searchLabel);
+        setMenuLabel(fields.menuLabel);
+        setLogoId(fields.logo.sys.id);
+      })
+      .catch((err) => console.log(err));
   }, [])
 
-  return (
-    <div>
+  useEffect(() => {
+    contentFullClient.getAsset(logoId)
+      .then((result) => {
+        setLogoUrl(`https:${result.fields.file.url}`)
+      })
+      .catch((err) => console.log(err));
+  }, [logoId])
 
-    </div>
+
+  return (
+    <>
+      <Header logoUrl={logoUrl} menuLabel={menuLabel} profileName="John Doe" />
+      <Container sx={{
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center'
+      }}>
+        <Box sx={{
+          padding: '60px 0'
+        }}>
+          <Typography sx={{
+            fontSize: '40px',
+            fontWeight: 'bold',
+            color: 'gray'
+          }}>
+            CredibleMind in the News
+          </Typography>
+        </Box>
+      </Container>
+    </>
   );
 }
 
